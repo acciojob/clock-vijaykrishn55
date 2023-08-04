c5b9ebba25046e111ff3503bc895927b8d96aa84
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-04 19:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function  livetime(){
+let date=new Date();
+    let time=date.toLocaleTimeString(`en-us`,{hour:'numeric',minute:'numeric',second:'numeric',hour12:true})
+    let p_date=date.getDay()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+    document.getElementById("timer").textContent= p_date+", "+time;
+    }
+function startTimer() {
+      livetime();
+      setInterval(livetime, 1000); 
+    }
+     
+    window.addEventListener('DOMContentLoaded', startTimer);
